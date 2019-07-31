1cb5bf76b66622972a5438b40474aea8a60e996c
wizard-jS <spraguej624@gmail.com>
2019-07-31 16:16:47 -0700

    Remove config module

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
-let config = require('config');
 /*
  * Use middle ware
  */
