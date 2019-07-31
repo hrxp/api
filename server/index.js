5a99657fd6fb4839cb4134a4d0e875296ceaba25
wizard-jS <spraguej624@gmail.com>
2019-07-31 16:13:50 -0700

    Set up loggin to not log during testing + house keeping

--- server/index.js
+++ server/index.js
@@ -3,22 +3,35 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
+let config = require('config');
+/*
+ * Use middle ware
+ */
 // Channel router
 const channel = require('./controller/channel.js');
 // Parses incoming requests with JSON payloads
 app.use(express.json());
-// 	HTTP request logger.
-app.use(morgan('dev'));
+// 	HTTP request logger if we are not in the test environment
+if (process.env.NODE_ENV !== 'test') {
+  // Use morgan to log at command line
+  app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
+}
 // Enable cross-origin resource sharing (CORS) with various options.
 app.use(cors());
-
+/*
+ * Routes
+ */
 app.get('/', (req, res) => {
   res.send('example endpoint');
 });
 
-// Routes
 app.use('/channels', channel);
 
-app.listen(3000, () => {
-  console.log('Hido ho, Captn! Listening on port 3000.');
+const server = app.listen(3000, () => {
+  if (process.env.NODE_ENV !== 'test') {
+    console.log('Hido ho, Captn! Listening on port 3000.');
+  }
 });
+
+// For testing
+module.exports = server;
