b1a9b286504bf564030694e13e8a4c9fd8fe1266
wizard-jS <spraguej624@gmail.com>
2019-07-31 16:16:32 -0700

    Fix mongo useCreateIndex error

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { mongoURL } = require('./config');
 
-const db = mongoose.connect(mongoURL, { useNewUrlParser: true });
+const db = mongoose.connect(mongoURL, { useNewUrlParser: true, useCreateIndex: true });
 
 db.then(() => console.log(`Connected to: hrxp-api-database`)).catch(err => {
   console.log(`There was a problem connecting to mongo at: ${mongoURL}`);
