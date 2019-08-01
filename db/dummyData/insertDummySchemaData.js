a4e41ff36d6dad8e7af5a709b749790475c0fc86
wizard-jS <spraguej624@gmail.com>
2019-07-31 20:18:52 -0700

    Refactor insert and delete functions to return a promise

--- db/dummyData/insertDummySchemaData.js
+++ db/dummyData/insertDummySchemaData.js
@@ -2,99 +2,101 @@ var mongoose = require('mongoose');
 const { mongoURL } = require('../config');
 const { Channel, User, Message } = require('../model/channel.js');
 
-const insertDummyData = async () => {
-  // make a connection
-  mongoose.connect(mongoURL, { useNewUrlParser: true });
+const insertDummyData = () => {
+  return new Promise((resolve, reject) => {
+    // make a connection
+    mongoose.connect(mongoURL, { useNewUrlParser: true });
 
-  // get reference to database
-  var db = mongoose.connection;
+    // get reference to database
+    var db = mongoose.connection;
 
-  db.on('error', console.error.bind(console, 'connection error:'));
+    db.on('error', console.error.bind(console, 'connection error:'));
 
-  await db.once('open', async () => {
-    try {
-      console.log('Connection Successful!');
+    db.once('open', async () => {
+      try {
+        // Dummy data
+        const dummyChannel = {
+          id: 'testChannel',
+          topic: 'general',
+          purpose: 'testChannel',
+          members: ['member1', 'member2'],
+          isArchived: 'yes',
+        };
 
-      // Dummy data
-      const dummyChannel = {
-        id: 'testChannel',
-        topic: 'general',
-        purpose: 'testChannel',
-        members: ['member1', 'member2'],
-        isArchived: 'yes',
-      };
+        const dummyMessage = {
+          id: 'testMessage',
+          ts: '10:00am',
+          text: 'example message',
+          channelId: 'testChannel',
+          files: [
+            {
+              id: 'aaaa',
+              displayName: 'cv',
+              fileType: 'pdf',
+              downloadUrl: 'http://download.com/pdf',
+            },
+          ],
+          replies: [{ id: 'a1234a', createdBy: 'user1', ts: '10:01am', text: 'example reply' }],
+        };
 
-      const dummyMessage = {
-        id: 'testMessage',
-        ts: '10:00am',
-        text: 'example message',
-        channelId: 'testChannel',
-        files: [
-          {
-            id: 'aaaa',
-            displayName: 'cv',
-            fileType: 'pdf',
-            downloadUrl: 'http://download.com/pdf',
-          },
-        ],
-        replies: [{ id: 'a1234a', createdBy: 'user1', ts: '10:01am', text: 'example reply' }],
-      };
+        const dummyUser = {
+          id: 'testUser',
+          profilePhoto: 'http://example.com/photo',
+          displayName: 'example name',
+          realName: 'John Doe',
+        };
 
-      const dummyUser = {
-        id: 'testUser',
-        profilePhoto: 'http://example.com/photo',
-        displayName: 'example name',
-        realName: 'John Doe',
-      };
+        // Insert one dummy document into the channel collection
+        await Channel.collection.insertOne(dummyChannel);
 
-      // Insert one dummy document into the channel collection
-      await Channel.collection.insertOne(dummyChannel);
+        // Insert one dummy document into the user collection
+        await User.collection.insertOne(dummyUser);
 
-      // Insert one dummy document into the user collection
-      await User.collection.insertOne(dummyUser);
+        // Insert on dummy document into the message collection
+        await Message.collection.insertOne(dummyMessage);
 
-      // Insert on dummy document into the message collection
-      await Message.collection.insertOne(dummyMessage);
-
-      console.log('Successfully inserted dummy data into collection');
-      mongoose.connection.close();
-    } catch (err) {
-      console.log('Error inserting dummy data: ', err);
-    }
+        console.log('Successfully inserted dummy data into collection');
+        mongoose.connection.close();
+        resolve();
+      } catch (err) {
+        console.log('Error inserting dummy data: ', err);
+        reject();
+      }
+    });
   });
-  return;
 };
 
-const deleteDummyData = async () => {
-  // make a connection
-  mongoose.connect(mongoURL, { useNewUrlParser: true });
+const deleteDummyData = () => {
+  return new Promise((resolve, reject) => {
+    // make a connection
+    mongoose.connect(mongoURL, { useNewUrlParser: true });
 
-  // get reference to database
-  var db = mongoose.connection;
+    // get reference to database
+    var db = mongoose.connection;
 
-  db.on('error', console.error.bind(console, 'connection error:'));
+    db.on('error', console.error.bind(console, 'connection error:'));
 
-  await db.once('open', async () => {
-    try {
-      console.log('Connection Successful!');
+    db.once('open', async () => {
+      try {
+        // dummy id's
+        const dummyChannelId = 'testChannel';
+        const dummyMessageId = 'testMessage';
+        const dummyUserId = 'testUser';
 
-      // dummy id's
-      const dummyChannelId = 'testChannel';
-      const dummyMessageId = 'testMessage';
-      const dummyUserId = 'testUser';
+        await Channel.deleteOne({ id: dummyChannelId }).catch(err => console.log(err));
+        await User.deleteOne({ id: dummyUserId });
+        await Message.deleteOne({ id: dummyMessageId });
 
-      await Channel.deleteOne({ id: dummyChannelId });
-      await User.deleteOne({ id: dummyUserId });
-      await Message.deleteOne({ id: dummyMessageId });
-
-      console.log('Successfully deleted dummy data');
-      mongoose.connection.close();
-      return;
-    } catch (err) {
-      console.log('Error deleting dummy data: ', err);
-    }
+        console.log('Successfully deleted dummy data');
+        mongoose.connection.close();
+        resolve();
+        return;
+      } catch (err) {
+        console.log('Error deleting dummy data: ', err);
+        reject();
+      }
+    });
   });
-  return;
 };
 
 module.exports = {
