90bc34a1605de7705846fd39afaaf82678f2f560
wizard-jS <spraguej624@gmail.com>
2019-08-01 14:35:04 -0700

    Add File Class to extreact file contents and save in the contructor

--- ingestor/utils.js
+++ ingestor/utils.js
@@ -13,3 +13,33 @@ const unzipFolder = (folderName, pathToExtract) => {
     unzipper.Extract({ path: __dirname + `/${pathToExtract}` })
   );
 };
+
+class File {
+  constructor(filename) {
+    this.filename = filename;
+    this.fileContents;
+  }
+
+  readFile() {
+    return new Promise((resolve, reject) => {
+      try {
+        fs.createReadStream(__dirname + '/channels.json')
+
+          .on('data', function(chunk) {
+            this.fileContents = JSON.parse(chunk.toString());
+          })
+
+          .on('close', function() {
+            resolve(this.fileContents);
+          });
+      } catch (err) {
+        reject();
+      }
+    });
+  }
+
+  async processFile(processFileFunction, ...otherArgs) {
+    const results = processFileFunction(...otherArgs);
+    return results;
+  }
+
