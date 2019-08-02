92ff648131e23ce59d646b5875cd86bde7789a85
wizard-jS <spraguej624@gmail.com>
2019-08-01 19:59:20 -0700

    Remove Insert & delete dummy data functions and replace with a connection defined in the describe validation

--- server/controller/tests/channels.test.js
+++ server/controller/tests/channels.test.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-const { Channel, Users, Message } = require('../db/model/channel');
-const { insertDummyData, deleteDummyData } = require('../db/dummyData/insertDummySchemaData');
-var express = require('express');
+const { Channel, User, Message } = require('../../../db/model/channel');
+const db = require('../../../db/index');
 
 //During the test the env variable is set to test
 process.env.NODE_ENV = 'test';
@@ -9,31 +8,82 @@ process.env.NODE_ENV = 'test';
 //Require the dev-dependencies
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const server = require('../server/index');
+const server = require('../../index');
 const should = chai.should();
 chai.use(chaiHttp);
 
 describe('channel', () => {
+  // Dummy data
+  const dummyChannel = {
+    id: 'testChannel',
+    topic: 'general',
+    purpose: 'testChannel',
+    members: ['member1', 'member2'],
+    isArchived: 'yes',
+  };
+
+  const dummyMessage = {
+    id: 'testMessage',
+    ts: '10:00am',
+    text: 'example message',
+    channelId: 'testChannel',
+    files: [
+      {
+        id: 'aaaa',
+        displayName: 'cv',
+        fileType: 'pdf',
+        downloadUrl: 'http://download.com/pdf',
+      },
+    ],
+    replies: [{ id: 'a1234a', createdBy: 'user1', ts: '10:01am', text: 'example reply' }],
+  };
+
+  const dummyUser = {
+    id: 'testUser',
+    profilePhoto: 'http://example.com/photo',
+    displayName: 'example name',
+    realName: 'John Doe',
+  };
+
+  let newChannel = new Channel(dummyChannel);
+  let newUser = new User(dummyUser);
+  let newMessage = new Message(dummyMessage);
+
   beforeEach(async () => {
-    await insertDummyData();
+    try {
+      await newChannel.save();
+      await newUser.save();
+      await newMessage.save();
+    } catch (err) {
+      err;
+    }
   });
-  
+
   afterEach(async () => {
-    await deleteDummyData();
+    const dummyChannelId = 'testChannel';
+    const dummyMessageId = 'testMessage';
+    const dummyUserId = 'testUser';
+    try {
+    } catch (err) {
+      await Channel.deleteOne({ id: dummyChannelId }).catch(err => console.log(err));
+      await User.deleteOne({ id: dummyUserId });
+      await Message.deleteOne({ id: dummyMessageId });
+    }
   });
 
   describe('All channels', () => {
-      it('it should GET all the channels', done => {
-        chai
-          .request(server)
-          .get('/channels')
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('array');
-            res.body[res.body.length - 1].id.should.equal('testChannel');
-            done();
-          });
-      });
+    it('it should GET all the channels', done => {
+      chai
+        .request(server)
+        .get('/channels')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('array');
+          res.body[res.body.length - 1].id.should.equal('testChannel');
+          server.close();
+          done();
+        });
+    });
   });
 
   describe('A channel messages', () => {
@@ -45,9 +95,9 @@ describe('channel', () => {
           res.should.have.status(200);
           res.body.should.be.a('array');
           // res.body[res.body.length - 1].id.should.equal('testMessage');
+
           done();
-     });
+        });
     });
   });
 });
-
