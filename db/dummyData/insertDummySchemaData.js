29be7107738d0cd8bb28bc98f293467bf9f3ea4d
wizard-jS <spraguej624@gmail.com>
2019-08-11 18:31:21 -0700

    Refactor insertDummy Data to insert many different channels, messages & users

--- db/dummyData/insertDummySchemaData.js
+++ db/dummyData/insertDummySchemaData.js
@@ -1,6 +1,71 @@
 var mongoose = require('mongoose');
+const faker = require('faker');
 const { mongoURL } = require('../config');
 const { Channel, User, Message } = require('../model/channel.js');
+
+const dummyChannels = () => {
+  const channels = [];
+
+  for (let i = 0; i < 20; i++) {
+    let channel = {
+      id: `testChannel${i}`,
+      topic: faker.hacker.noun(),
+      purpose: faker.hacker.phrase(),
+      members: ['member1', 'member2'],
+      isArchived: 'yes',
+    };
+    channels.push(channel);
+  }
+
+  return channels;
+};
+
+const dummyMessages = () => {
+  const messages = [];
+
+  for (let i = 0, j = 0; i < 100; i++, j++) {
+    let message = {
+      id: `testMessageId${i}`,
+      ts: '10:00am',
+      text: faker.hacker.phrase(),
+      channelId: `testChannel${j}`,
+      files: [
+        {
+          id: `testFile${i}`,
+          displayName: 'cv',
+          fileType: 'pdf',
+          downloadUrl: 'http://download.com/pdf',
+        },
+      ],
+      replies: [
+        { id: `testReply${i}`, createdBy: 'user1', ts: '10:01am', text: faker.hacker.phrase() },
+      ],
+    };
+    messages.push(message);
+    // If testChannelId === 19, reset the variable to 0.
+    if (j === 19) {
+      j = 0;
+    }
+  }
+
+  return messages;
+};
+
+const dummyUsers = () => {
+  const users = [];
+
+  for (let i = 0; i < 20; i++) {
+    let user = {
+      id: `testUser${i}`,
+      profilePhoto: faker.image.avatar(),
+      displayName: faker.hacker.noun(),
+      realName: 'John Doe',
+    };
+    users.push(user);
+  }
+
+  return users;
+};
 
 const insertDummyData = () => {
   return new Promise((resolve, reject) => {
@@ -47,20 +112,20 @@ const insertDummyData = () => {
         };
 
         // Insert one dummy document into the channel collection
-        await Channel.collection.insertOne(dummyChannel);
+        await Channel.collection.insertMany(dummyChannels());
 
         // Insert one dummy document into the user collection
-        await User.collection.insertOne(dummyUser);
+        await User.collection.insertMany(dummyUsers());
 
         // Insert on dummy document into the message collection
-        await Message.collection.insertOne(dummyMessage);
+        await Message.collection.insertMany(dummyMessages());
 
         console.log('Successfully inserted dummy data into collection');
         mongoose.connection.close();
         resolve();
       } catch (err) {
         console.log('Error inserting dummy data: ', err);
-        reject();
+        reject(err);
       }
     });
   });
@@ -70,7 +135,7 @@ const deleteDummyData = () => {
   return new Promise((resolve, reject) => {
     // Make a connection
     mongoose.connect(mongoURL, { useNewUrlParser: true });
-
+    console.log('hi');
     // Get reference to database
     var db = mongoose.connection;
 
@@ -102,3 +167,5 @@ module.exports = {
   insertDummyData,
   deleteDummyData,
 };
+
+insertDummyData();
