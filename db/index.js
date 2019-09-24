eed72934e819f01947bb309dc2ae7479bc1815bc
kyle <shockey@users.noreply.github.com>
2019-09-23 19:06:29 -0700

    chore: update Prettier configuration (#22)
    * remove `printWidth` and `singleQuote` options
    
    * run prettier

--- db/index.js
+++ db/index.js
@@ -1,6 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const db = mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useCreateIndex: true });
+const db = mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
 
 db.then(() => console.log(`Connected to: hrxp-api-database`)).catch(err => {
   console.log(`There was a problem connecting to mongo at: ${mongoURL}`);
--- server/controller/tests/channels.test.js
+++ server/controller/tests/channels.test.js
@@ -1,47 +1,47 @@
-const mongoose = require('mongoose');
-const { Channel, User, Message } = require('../../../db/model/channel');
-const db = require('../../../db/index');
+const mongoose = require("mongoose");
+const { Channel, User, Message } = require("../../../db/model/channel");
+const db = require("../../../db/index");
 
 //During the test the env variable is set to test
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = "test";
 
 //Require the dev-dependencies
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const server = require('../../index');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../../index");
 const should = chai.should();
 chai.use(chaiHttp);
 
-describe('channel', () => {
+describe("channel", () => {
   // Dummy data
   const dummyChannel = {
-    id: 'testChannel',
-    topic: 'general',
-    purpose: 'testChannel',
-    members: ['member1', 'member2'],
-    isArchived: 'yes',
+    id: "testChannel",
+    topic: "general",
+    purpose: "testChannel",
+    members: ["member1", "member2"],
+    isArchived: "yes",
   };
 
   const dummyMessage = {
-    id: 'testMessage',
-    ts: '10:00am',
-    text: 'example message',
-    channelId: 'testChannel',
+    id: "testMessage",
+    ts: "10:00am",
+    text: "example message",
+    channelId: "testChannel",
     files: [
       {
-        id: 'aaaa',
-        displayName: 'cv',
-        fileType: 'pdf',
-        downloadUrl: 'http://download.com/pdf',
+        id: "aaaa",
+        displayName: "cv",
+        fileType: "pdf",
+        downloadUrl: "http://download.com/pdf",
       },
     ],
   };
 
   const dummyUser = {
-    id: 'testUser',
-    profilePhoto: 'http://example.com/photo',
-    displayName: 'example name',
-    realName: 'John Doe',
+    id: "testUser",
+    profilePhoto: "http://example.com/photo",
+    displayName: "example name",
+    realName: "John Doe",
   };
 
   beforeEach(async () => {
@@ -54,47 +54,47 @@ describe('channel', () => {
       await newUser.save();
       await newMessage.save();
     } catch (err) {
-      console.log('Error before each: ', err);
+      console.log("Error before each: ", err);
     }
   });
 
   afterEach(async () => {
     // TODO: End the db connection before every test
-    const dummyChannelId = 'testChannel';
-    const dummyMessageId = 'testMessage';
-    const dummyUserId = 'testUser';
+    const dummyChannelId = "testChannel";
+    const dummyMessageId = "testMessage";
+    const dummyUserId = "testUser";
     try {
       await Channel.deleteOne({ id: dummyChannelId });
       await User.deleteOne({ id: dummyUserId });
       await Message.deleteOne({ id: dummyMessageId });
     } catch (err) {
-      console.log('error after each: ', err);
+      console.log("error after each: ", err);
     }
   });
 
-  describe('All channels', () => {
-    it('it should GET all the channels', done => {
+  describe("All channels", () => {
+    it("it should GET all the channels", done => {
       chai
         .request(server)
-        .get('/channels')
+        .get("/channels")
         .end((err, res) => {
           res.should.have.status(200);
-          res.body[res.body.length - 1].id.should.equal('testChannel');
+          res.body[res.body.length - 1].id.should.equal("testChannel");
           done();
         });
     });
   });
 
-  describe('A channel messages', () => {
-    const dummyChannelId = 'testChannel';
-    it('it should GET all the messages for a channel', done => {
+  describe("A channel messages", () => {
+    const dummyChannelId = "testChannel";
+    it("it should GET all the messages for a channel", done => {
       chai
         .request(server)
         .get(`/channels/${dummyChannelId}/messages`)
         .end((err, res) => {
           res.should.have.status(200);
-          res.body.should.be.a('array');
-          res.body[0].id.should.equal('testMessage1');
+          res.body.should.be.a("array");
+          res.body[0].id.should.equal("testMessage1");
           done();
         });
     });
